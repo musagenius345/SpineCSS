5abcf6545400c0dc664a8c73deec21e31eecd43d
musagenius345 <musabadru@gmail.com>
2023-04-27 00:26:17 +0300

    colors custom properties

--- postcss.config.js
+++ postcss.config.js
@@ -5,8 +5,24 @@ module.exports = {
         discardComments: {
           removeAll: true,
           svgo: false,
-        },
+        }
       }]
+    }),
+      require('postcss-preset-env')({
+      stage: 0,
+      features: {
+        'logical-properties-and-values': false,
+        'prefers-color-scheme-query': false,
+        'gap-properties': false,
+        'custom-properties': false,
+        'place-properties': false,
+        'not-pseudo-class': false,
+        'focus-visible-pseudo-class': false,
+        'focus-within-pseudo-class': false,
+        'color-functional-notation': false,
+        // 'custom-media-queries': {},
+        'double-position-gradients': false,
+      }
     })
   ]
 }
--- src/model/scss.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  source: './data/templates/template.scss',
-  target: './sass/pesticide.scss',
-  entry: '  %s { outline: 1px solid %s !important; }'
-};
